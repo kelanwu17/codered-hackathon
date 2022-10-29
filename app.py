5e3570915c47f80edc9b66a64b5f2086a04464ff
Kelan Wu <kelanwu17@gmail.com>
2022-10-29 14:43:42 -0500

    fix api

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ from flask import Flask, render_template
 import requests
 import json
 api_key = "lLhgwKk3K0bBy3WCT8kUPbNErsjjRcJjO8h5ZUs3"
-response = requests.get("https://api.eia.gov/v2/natural-gas/prod/oilwells/data/?api_key=lLhgwKk3K0bBy3WCT8kUPbNErsjjRcJjO8h5ZUs3")
+response = requests.get("https://api.eia.gov/v2/natural-gas/prod/wells/data/?api_key=" + api_key + "&data[]=value")
 
 def jprint(obj):
     text = json.dumps(obj, sort_keys=True, indent= 4)
@@ -14,7 +14,7 @@ print(response.status_code)
 
 # @app.route("/")
 # def home():
-#     return "<p>Hello<p>"
+#     return render_template('index.html')
 
 # if __name__ == "__main__":
 #     app.run()
