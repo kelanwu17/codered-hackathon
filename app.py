bc99c42fc8211fa3b8f1388a8d4ebc306206978d
Kelan Wu <kelanwu17@gmail.com>
2022-10-29 18:42:43 -0500

    fjfjjffjf

--- app.py
+++ app.py
@@ -1,22 +1,53 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, Markup
 import requests
 import json
+import pandas
+
+#loading Oil Wells Data
 api_key = "lLhgwKk3K0bBy3WCT8kUPbNErsjjRcJjO8h5ZUs3"
 response = requests.get("https://api.eia.gov/v2/natural-gas/prod/wells/data/?api_key=" + api_key + "&data[]=value")
 
 def jprint(obj):
     text = json.dumps(obj, sort_keys=True, indent= 4)
     print(text)
+    
 
 json_data = json.loads(response.text)
 jprint(json_data["response"]["data"][0])
+#inputing data into lists
+texas_value = []
+texas_period = []
+for i in range(0, len(json_data["response"]["data"])):
+    if json_data["response"]["data"][i]["area-name"] == "TEXAS":
+        texas_value.append(json_data["response"]["data"][i]["value"])
+        texas_period.append(json_data["response"]["data"][i]["period"])
 
+#sorting the lists
+sorted = False
+while (sorted != True):
+    sorted = True
+    for i in range(0, len(texas_value) - 1):
+        if texas_period[i] > texas_period[i+1]:
+            temp_period = texas_period[i+1]
+            texas_period[i+1] = texas_period[i]
+            texas_period[i] = temp_period
+            
+            temp_value = texas_value[i+1]
+            texas_value[i+1] = texas_value[i]
+            texas_value[i] = temp_value
+            sorted = False
 
-# app = Flask(__name__)
+app = Flask(__name__)
 
 # @app.route("/")
 # def home():
 #     return render_template('index.html')
 
-# if __name__ == "__main__":
-#     app.run()
+@app.route('/line')
+def line():
+    line_labels=texas_period
+    line_values=texas_value
+    return render_template('index.html', title='Number of Gas Producing Oil Wells in Texas', max=17000, labels=texas_period, values=texas_value)
+
+if __name__ == "__main__":
+    app.run(debug=True)
